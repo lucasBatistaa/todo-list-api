d76b147f1173a980f5fba4a648eada8276f8155d
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-11-15 15:52:58 -0300

    add prisma errors to errorHandler middleware

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 import { ClientError } from "../errors/clientError";
+import { Prisma } from "@prisma/client";
 
 export default function errorHandler(
   err: Error,
@@ -8,18 +9,28 @@ export default function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  if (err instanceof ZodError) {
-    res.status(400).json({
-      message: "Erro na validação de dados!",
-      error: err.flatten().fieldErrors,
-    });
-  } else if (err instanceof ClientError) {
-    res.status(400).json({
-      message: err.message,
-    });
-  } else {
-    res.status(500).json({
-      message: "Erro interno no servidor",
-    });
-  }
+	if (err instanceof ZodError) {
+		res.status(400).json({
+			message: "Erro na validação de dados!",
+			error: err.flatten().fieldErrors,
+		});
+	} else if (err instanceof ClientError) {
+		res.status(400).json({
+			message: err.message,
+		});
+	} else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+		res.status(400).json({
+			message: `Erro na requisição ao banco de dados: ${err.message}`,
+			code: err.code, 
+		});
+	} else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+		res.status(500).json({
+			message: "Erro desconhecido ao acessar o banco de dados",
+			error: err.message,
+		});
+	} else {
+		res.status(500).json({
+		message: "Erro interno no servidor",
+		});
+	}
 }
